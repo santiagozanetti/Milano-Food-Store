1536ba6292345adf0dc0def8655b6437b04629fe
Santiago <santiango.zanetti@cardenalspionla.com.ar>
2024-01-05 14:06:59 -0300

    Agrego funcion del menu

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 const menu = document.querySelector(".hamburguesa");
 const navegacion = document.querySelector(".navegacion");
 const imagenes = document.querySelectorAll("img");
+const btnTodos = document.querySelector(".todos");
+const btnEnsaladas = document.querySelector(".ensaladas");
+const btnPasta = document.querySelector(".pasta");
+const btnPizza = document.querySelector(".pizza");
+const btnPostres = document.querySelector(".postres");
+const contenedorPlatos = document.querySelector(".platos");
 
 document.addEventListener("DOMContentLoaded", () => {
   eventos();
+  platos();
 });
 
 const eventos = () => {
@@ -28,11 +35,19 @@ const botonCerrar = () => {
   cerrarMenu(btnCerrar, overlay);
 };
 
-
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const imagen = entry.target;
+      imagen.src = imagen.dataset.src; // mostrar solo las imagenes cuando se interactua con ellas.
+      observer.unobserve(imagen);
+    }
+  });
+});
 
 // Asigna el valor del atributo 'data-src' al atributo 'src' de cada imagen en el array 'imagenes'
 imagenes.forEach((imagen) => {
-  imagen.src = imagen.dataset.src;
+  observer.observe(imagen);
 });
 
 const cerrarMenu = (boton, overlay) => {
@@ -47,4 +62,65 @@ const cerrarMenu = (boton, overlay) => {
     navegacion.classList.add("ocultar");
     boton.remove();
   };
+};
+
+const platos = () => {
+  let platosArreglo = []; // arreglo para platos
+  const platos = document.querySelectorAll(".plato");
+
+  platos.forEach((plato) => (platosArreglo = [...platosArreglo, plato])); // copiar arreglo y hago referencia para recorrerlo y meter platos en el arreglo.
+
+  //.filter genera un nuevo arreglo con lo que estamos buscando, en este caso el valor "ensalada"
+
+  const ensaladas = platosArreglo.filter(
+    (ensalada) => ensalada.getAttribute("data-plato") === "ensalada"
+  );
+
+  const pastas = platosArreglo.filter(
+    (pasta) => pasta.getAttribute("data-plato") === "pasta"
+  );
+
+  const pizzas = platosArreglo.filter(
+    (pizza) => pizza.getAttribute("data-plato") === "pizza"
+  );
+
+  const postres = platosArreglo.filter(
+    (postre) => postre.getAttribute("data-plato") === "postre"
+  );
+
+  mostrarPlatos(ensaladas, pastas, pizzas, postres, platosArreglo);
+};
+
+const mostrarPlatos = (ensaladas, pastas, pizzas, postres, todos) => {
+  btnEnsaladas.addEventListener("click", () => {
+    limpiarHtml(contenedorPlatos);
+    ensaladas.forEach((ensalada) => contenedorPlatos.appendChild(ensalada));
+  });
+
+  btnPasta.addEventListener("click", () => {
+    limpiarHtml(contenedorPlatos);
+    pastas.forEach((pasta) => contenedorPlatos.appendChild(pasta));
+  });
+
+  btnPizza.addEventListener("click", () => {
+    limpiarHtml(contenedorPlatos);
+    pizzas.forEach((pizza) => contenedorPlatos.appendChild(pizza));
+  });
+
+  btnPostres.addEventListener("click", () => {
+    limpiarHtml(contenedorPlatos);
+    postres.forEach((postre) => contenedorPlatos.appendChild(postre));
+  });
+
+  btnTodos.addEventListener("click", () => {
+    limpiarHtml(contenedorPlatos);
+    todos.forEach((todo) => contenedorPlatos.appendChild(todo));
+  });
+};
+
+// mientras haya contenido que nos elimine el anterior
+const limpiarHtml = (contenedor) => {
+  while (contenedor.firstChild) {
+    contenedor.removeChild(contenedor.firstChild);
+  }
 };
